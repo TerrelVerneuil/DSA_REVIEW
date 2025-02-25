45b91dada30f4df9ac9968aba586629d8a6fab29
TerrelVerneuil <44128398+TerrelVerneuil@users.noreply.github.com>
2025-02-25 10:56:47 -0500

    added more empty python files for DSA

---
+++ heap.py
@@ -0,0 +1,11 @@
+# A Heap is a complete binary tree data structure 
+# that satisfies the heap property: for every node, 
+# the value of its children is greater than or equal 
+# to its own value. Heaps are usually used to 
+# implement priority queues, where the smallest 
+# (or largest) element is always at the root of the tree.
+
+# Binary Heap is a complete binary tree that satisfies the heap property.
+class Heap:
+    def __init__(self):
+        pass
